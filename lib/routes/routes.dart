6450fb56cf01d89d1511625c799c73df2a44b0db
agarciao <agarciao@iacs.es>
2022-05-04 09:27:46 +0200

    Añadida firstquestion

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:app_antibioticos/screens/screens.dart';
-import 'package:app_antibioticos/screens/pruebahome.dart';
 
 final Map<String, Widget Function(BuildContext)> appRoutes = {
   'ranking': (_) => const Ranking(),
   'home': (_) => const HomeScreen(),
-  'firstquestion': (_) => const FirstQuestion(),
-  'prueba': (_) => const Prueba()
+  'firstquestion': (_) => const FirstQuestion()
 };
