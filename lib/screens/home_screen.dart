dfc9efb06910a0b6c964ddc8df15767d0c76d443
agarciao <agarciao@iacs.es>
2022-05-18 10:45:33 +0200

    añadido eliminador de tildes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 import 'package:app_antibioticos/screens/screens.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../utilidades/constantes.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getBackpack();
+  }
+
+  Future getBackpack() async {
+    Map data;
+    http.Response response =
+        await http.get(Uri.parse(conexion1 + "/api/backpack"));
+    data = json.decode(response.body);
+
+    setState(() {
+      mochilaSeleccionada = data['backpack'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/treatment/treatment.dart
+++ lib/screens/treatment/treatment.dart
@@ -69,43 +69,6 @@ class TreatmentState extends State<TreatmentScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Segunda pregunta"),
-        actions: [
-          //Boton que muestra la informacion inicial del caso
-          IconButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const ModalInfo(
-                      title: 'Info inicial',
-                      info:
-                          'Ex qui ullamco ut labore cupidatat sit aliquip eiusmod quis nostrud esse ea esse. Elit commodo ipsum et dolor magna amet dolor cupidatat esse sint Lorem aute incididunt cupidatat. Sunt consequat exercitation ad exercitation incididunt ex laboris officia dolore.');
-                },
-              );
-            },
-            icon: const Icon(Icons.info_outline_rounded),
-          ),
-          //Espacio entre los botones
-          const SizedBox(width: 20),
-          //Boton que muestra lo seleccionado en la pregunta anterior + feedbacks
-          IconButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const ModalInfo(
-                      title: 'Feedback primera pregunta',
-                      info:
-                          'Ex qui ullamco ut labore cupidatat sit aliquip eiusmod quis nostrud esse ea esse. Elit commodo ipsum et dolor magna amet dolor cupidatat esse sint Lorem aute incididunt cupidatat. Sunt consequat exercitation ad exercitation incididunt ex laboris officia dolore.');
-                },
-              );
-            },
-            icon: const Icon(Icons.feedback_outlined),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 30),
-          ),
-        ],
       ),
       body: SingleChildScrollView(
         child: Column(
