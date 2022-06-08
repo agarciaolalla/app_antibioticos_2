0a5182ad1c92da2ddeaae88695c0d125697589bf
agarciao <agarciao@iacs.es>
2022-06-08 14:28:54 +0200

    arreglado ranking

--- lib/screens/ranking.dart
+++ lib/screens/ranking.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:app_antibioticos/models/models.dart';
-import 'package:app_antibioticos/services/player_peticion.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:app_antibioticos/utilidades/constantes.dart';
 
 class Ranking extends StatefulWidget {
   const Ranking({Key? key}) : super(key: key);
@@ -9,111 +12,97 @@ class Ranking extends StatefulWidget {
   State<StatefulWidget> createState() => HomeRanking();
 }
 
-List<Player> playersRank = [];
-
 class HomeRanking extends State<Ranking> {
   @override
+  void initState() {
+    super.initState();
+    getPlayers();
+  }
+
+  List jugadores = [];
+  List playersRank = [];
+
+  //Te recoge la respuesta de la base de datos.
+  Future getPlayers() async {
+    Map data;
+    http.Response response =
+        await http.get(Uri.parse(conexion1 + "/api/player"));
+    // debugPrint(response.body);
+    data = json.decode(response.body);
+
+    setState(() {
+      jugadores = data['player'];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    orderPlayers();
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: SafeArea(
-          child: Column(
-            children: const [
-              Text(
-                "Ranking Jugadores",
-                style: TextStyle(
-                    fontSize: 25, fontFamily: 'FjallaOne', color: Colors.black),
-              ),
-            ],
+        appBar: AppBar(
+          flexibleSpace: SafeArea(
+            child: Column(
+              children: const [
+                Text(
+                  "Ranking Jugadores",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'FjallaOne',
+                      color: Colors.black),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      body: getClients(context, listPlayer()),
-    );
-  }
-
-  Widget getClients(BuildContext context, Future<List<Player>> futureClient) {
-    return FutureBuilder(
-      future: futureClient,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        switch (snapshot.connectionState) {
-          //En este case estamos a la espera de la respuesta, mientras tanto mostraremos el cargando...
-          case ConnectionState.waiting:
-            return const Center(child: CircularProgressIndicator());
-
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              return Container(
-                alignment: Alignment.center,
-                child: Center(
-                  child: Text('Error: ${snapshot.error}'),
-                ),
-              );
-            }
-            // print(snapshot.data);
-            return snapshot.data != null
-                ? clientList(snapshot.data)
-                : Container(
-                    alignment: Alignment.center,
-                    child: const Center(
-                      child: Text('Sin Datos'),
-                    ),
-                  );
-          default:
-            return const Text('Recarga la pantalla....!');
-        }
-      },
-    );
-  }
-
-  Widget clientList(List<Player> jugadores) {
-    orderPlayers(jugadores);
-    return ListView.builder(
-      itemCount: playersRank.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(
-            playersRank[index].name + " " + playersRank[index].surname,
-            style: const TextStyle(color: Colors.black, fontSize: 17),
-          ),
-          subtitle: Text(
-            "Retos completados: " +
-                playersRank[index].challenges +
-                "\nVida: " +
-                playersRank[index].life +
-                "%" +
-                "\nAntibioticos Restantes: " +
-                playersRank[index].medicines +
-                "\nPuntuacion Diagnostica: " +
-                playersRank[index].points +
-                " puntos",
-            style: const TextStyle(color: Colors.black, fontSize: 17),
-          ),
-          leading: CircleAvatar(
-            child: Text((index + 1).toString()),
-          ),
-        );
-      },
-    );
+        body: ListView.builder(
+          itemCount: playersRank.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                playersRank[index]["nombre"] +
+                    " " +
+                    playersRank[index]["apellido"],
+                style: const TextStyle(color: Colors.black, fontSize: 17),
+              ),
+              subtitle: Text(
+                "Retos completados: " +
+                    playersRank[index]["retoscompletados"] +
+                    "\nVida: " +
+                    playersRank[index]["vida"] +
+                    "%" +
+                    "\nAntibioticos Restantes: " +
+                    playersRank[index]["antibioticostotales"] +
+                    "\nPuntuacion Diagnostica: " +
+                    playersRank[index]["puntuacion"] +
+                    " puntos",
+                style: const TextStyle(color: Colors.black, fontSize: 17),
+              ),
+              leading: CircleAvatar(
+                child: Text((index + 1).toString()),
+              ),
+            );
+          },
+        ));
   }
 
   //Metodo para ordenar a los jugadores en el ranking
-  void orderPlayers(List<Player> jugadores) {
+  void orderPlayers() {
     //Añadimos solamente los jugadores que hayan completado los 5 casos
     for (int i = 0; i < jugadores.length; i++) {
-      if (jugadores[i].challenges == "5") {
+      if (jugadores[i]["retoscompletados"] == "5") {
         playersRank.add(jugadores[i]);
       }
     }
 
     //Ordenamos los jugadores con 5 casos completados con los parametros solicitados.
     playersRank.sort((a, b) {
-      int vida = double.parse(b.life).compareTo(double.parse(a.life));
+      int vida = double.parse(b["vida"]).compareTo(double.parse(a["vida"]));
       if (vida == 0) {
-        int antibioticos =
-            int.parse(b.medicines).compareTo(int.parse(a.medicines));
+        int antibioticos = int.parse(b["antibioticostotales"])
+            .compareTo(int.parse(a["antibioticostotales"]));
         if (antibioticos == 0) {
-          return int.parse(b.points).compareTo(int.parse(a.medicines));
+          return int.parse(b.points)
+              .compareTo(int.parse(a["antibioticostotales"]));
         }
         return antibioticos;
       }
@@ -121,21 +110,24 @@ class HomeRanking extends State<Ranking> {
     });
 
     //Creamos otro array donde meteremos los que tienen < 5 casos completados
-    List<Player> playersFail = [];
+    List playersFail = [];
 
     for (int i = 0; i < jugadores.length; i++) {
-      if (int.parse(jugadores[i].challenges) < 5) {
+      if (int.parse(jugadores[i]["retoscompletados"]) < 5) {
         playersFail.add(jugadores[i]);
       }
     }
 
     //Ordenamos los jugadores con menos de 5 casos completados con los parametros solicitados
     playersFail.sort((a, b) {
-      int retos = int.parse(b.challenges).compareTo(int.parse(a.challenges));
+      int retos = int.parse(b["retoscompletados"])
+          .compareTo(int.parse(a["retoscompletados"]));
       if (retos == 0) {
-        int puntos = int.parse(b.points).compareTo(int.parse(a.points));
+        int puntos =
+            int.parse(b["puntuacion"]).compareTo(int.parse(a["puntuacion"]));
         if (puntos == 0) {
-          return int.parse(b.medicines).compareTo(int.parse(a.medicines));
+          return int.parse(b["antibioticostotales"])
+              .compareTo(int.parse(a["antibioticostotales"]));
         }
         return puntos;
       }
